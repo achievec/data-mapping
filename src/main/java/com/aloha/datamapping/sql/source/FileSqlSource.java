e2a18e6c31eb56bf9a6ab49b1ad64c2fdf8fe2d1
achievec <achievec@gmail.com>
2018-02-09 16:16:53 +0800

    use commons#fileutils to read file sql source

--- src/main/java/com/aloha/datamapping/sql/source/FileSqlSource.java
+++ src/main/java/com/aloha/datamapping/sql/source/FileSqlSource.java
@@ -1,10 +1,14 @@
 package com.aloha.datamapping.sql.source;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.io.File;
+import java.nio.charset.Charset;
 
 @Data
 @AllArgsConstructor
@@ -13,6 +17,6 @@ public class FileSqlSource implements SqlSource {
     private String path;
 
     public String getSource() throws Exception {
-        return IOUtils.toString(new ClassPathResource(path).getInputStream());
+        return FileUtils.readFileToString(new File(path), Charset.forName("utf-8"));
     }
 }
